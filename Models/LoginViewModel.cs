eaf203f290f028b7cf61eefae5a11fbcfb0cf94e
nuno chev <nunochev@live.com>
2020-10-17 12:41:57 +0200

    Model LOGIN

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stock_Sensor.Models
+{
+    public class LoginViewModel
+    {
+        public string Utilizador { get; set; }
+        public string Senha { get; set; }
+        public bool LembrarMe { get; set; }
+    }
+}
