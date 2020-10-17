3fec1d64faa4b365814052cfc6088f86bc76517e
nuno chev <nunochev@live.com>
2020-10-17 23:32:13 +0200

    Model do Grupo Produto

---
+++ Models/GrupoProdutoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+
+namespace Stock_Sensor.Models
+{
+    public class GrupoProdutoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
