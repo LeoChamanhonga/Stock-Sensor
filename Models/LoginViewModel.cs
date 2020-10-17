82026de5dd566d42ab864fd00c7e7de6cec0eedb
nuno chev <nunochev@live.com>
2020-10-17 14:39:34 +0200

    Tratamento de campos obrigatorios

--- Models/LoginViewModel.cs
+++ Models/LoginViewModel.cs
@@ -8,8 +8,11 @@ namespace Stock_Sensor.Models
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage ="Informe o utilizador")]
         [Display(Name ="Utilizador")]
         public string Utilizador { get; set; }
+        [Required(ErrorMessage ="Informe a Senha!")]
+        [DataType(DataType.Password)]
         public string Senha { get; set; }
         public bool LembrarMe { get; set; }
     }
