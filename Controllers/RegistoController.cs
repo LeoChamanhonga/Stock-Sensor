5d1b459734dbef1a4e2d7edd74623fb7ea6e7b62
nuno chev <nunochev@live.com>
2020-10-17 11:03:38 +0200

    Controller REGISTO

---
+++ Controllers/RegistoController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Stock_Sensor.Controllers
+{
+    public class RegistoController : Controller
+    {
+        // GET: Registo
+        public ActionResult GrupoProduto()
+        {
+            return View();
+        }
+
+        public ActionResult MarcaProduto()
+        {
+            return View();
+        }
+        public ActionResult LocalArmazenamento()
+        {
+            return View();
+        }
+        public ActionResult UnidadeMedida()
+        {
+            return View();
+        }
+        public ActionResult Produto()
+        {
+            return View();
+        }
+        public ActionResult Pais()
+        {
+            return View();
+        }
+        public ActionResult Provincia()
+        {
+            return View();
+        }
+        public ActionResult Cidade()
+        {
+            return View();
+        }
+
+        public ActionResult Fornecedor()
+        {
+            return View();
+        }
+        public ActionResult PerfilUtilizador()
+        {
+            return View();
+        }
+        public ActionResult Utilizador()
+        {
+            return View();
+        }
+
+    }
+}
