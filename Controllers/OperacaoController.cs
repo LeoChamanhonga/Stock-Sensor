2505283961b2d6179db75b3fb2602718b887b832
nuno chev <nunochev@live.com>
2020-10-17 11:23:23 +0200

    Controlers (Grafico, Operacao e Relatorio)

---
+++ Controllers/OperacaoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Stock_Sensor.Controllers
+{
+    public class OperacaoController : Controller
+    {
+        // GET: Operacao
+        public ActionResult EntradaStock()
+        {
+            return View();
+        }
+        public ActionResult SaidaStock()
+        {
+            return View();
+        }
+        public ActionResult LancPerdas()
+        {
+            return View();
+        }
+        public ActionResult Inventarios()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/RelatorioController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Stock_Sensor.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        // GET: Relatorio
+        public ActionResult EstadoStock()
+        {
+            return View();
+        }
+        public ActionResult Encomenda()
+        {
+            return View();
+        }
+
+    }
+}
